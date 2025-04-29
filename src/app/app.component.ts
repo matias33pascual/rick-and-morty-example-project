bf910d02caca205a96f43ee5ad01ce9b11ec6f6f
Matias Pascual <62584633+matias33pascual@users.noreply.github.com>
2025-04-29 19:06:58 -0300

    adding styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
 
 @Component({
