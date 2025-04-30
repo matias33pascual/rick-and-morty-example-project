afc4cea4913bf47563b402c5d234fde0f68c6852
Matias Pascual <62584633+matias33pascual@users.noreply.github.com>
2025-04-30 10:31:28 -0300

    fix close button in character-detail

--- src/app/pages/home/characters-detail/characters-detail.component.ts
+++ src/app/pages/home/characters-detail/characters-detail.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Character } from '../../../core/characters/models/character.interface';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-characters-detail',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './characters-detail.component.html',
   styleUrl: './characters-detail.component.css',
 })
 export class CharactersDetailComponent {
   @Input() character: Character | null = null;
+  @Output() close = new EventEmitter<void>();
+
+  onClose(): void {
+    this.close.emit();
+  }
 }
