35d4de53a1b2c649617ef579dafbfc694f6bcbbd
Matias Pascual <62584633+matias33pascual@users.noreply.github.com>
2025-04-30 15:18:41 -0300

    fixing bug in favorites

--- src/app/core/favorites/store/favorites.actions.ts
+++ src/app/core/favorites/store/favorites.actions.ts
@@ -1,12 +1,15 @@
+import { createAction } from '@ngrx/store';
 import { Character } from '../../characters/models';
 import { FavoritesActionTypes } from '../models/favorites-action-types.enum';
 
-export const addFavorite = (character: Character) => ({
-  type: FavoritesActionTypes.AddFavorite,
-  character,
-});
+export const addFavorite = createAction(FavoritesActionTypes.AddFavorite, (character: Character) => ({
+  payload: character,
+}));
 
-export const removeFavorite = (characterId: number) => ({
-  type: FavoritesActionTypes.RemoveFavorite,
-  characterId,
-});
+export const removeFavorite = createAction(FavoritesActionTypes.RemoveFavorite, (characterId: number) => ({
+  payload: characterId,
+}));
+
+export const loadFavorites = createAction(FavoritesActionTypes.LoadFavorites);
+
+export const saveFavorites = createAction(FavoritesActionTypes.SaveFavorites);
